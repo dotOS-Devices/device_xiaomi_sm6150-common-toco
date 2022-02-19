1aa94a56f7cad2a1916dc35212aecd14806546ca
AndroidHQ254 <d.murimi14@gmail.com>
2022-02-19 12:59:55 +0300

    sm6150-common: Build libxml2.vendor package

--- sm6150.mk
+++ sm6150.mk
@@ -100,6 +100,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
+    libxml2.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
