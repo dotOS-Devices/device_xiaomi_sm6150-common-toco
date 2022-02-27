bf4bf25dadedafc477c72ee181d606aaca0b93da
AndroidHQ254 <d.murimi14@gmail.com>
2022-02-27 14:30:00 +0300

    sm6150-common: Drop SnapCam

--- sm6150.mk
+++ sm6150.mk
@@ -118,9 +118,6 @@ PRODUCT_PACKAGES += \
     libxml2.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
-PRODUCT_PACKAGES += \
-    Snap
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
