ecc7146efd1436653f94e93c796859d71ca40bcd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-28 19:29:12 +0300

    sm6150-common: libinit: Import changes from sm8250-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,6 +93,10 @@ DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_mani
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/hidl/compatibility_matrix.xml
 ODM_MANIFEST_FILES += $(COMMON_PATH)/configs/hidl/manifest-qva.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_sm6150
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_sm6150
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
