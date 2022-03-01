3b98467caca884aab1bf980f7895c538d4f9e3be
yukiscape <garikapatiamartya@gmail.com>
2022-03-01 13:36:14 +0300

    sm6150-common: Add hardware/qcom-caf/wlan to namespaces and wpa_supplicant_8_lib to cfi path

--- sm6150.mk
+++ sm6150.mk
@@ -364,7 +364,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/qcom-caf/wlan \
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -460,6 +461,10 @@ PRODUCT_PACKAGES += \
 
 # WfdCommon
 -include vendor/xiaomi/sm6150-common-extra/wfd.mk
+
+# Wlan
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
