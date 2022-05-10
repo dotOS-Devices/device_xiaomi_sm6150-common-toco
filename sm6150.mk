c80e21773daf17c43e797ebb0f766f9b09872425
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2022-05-11 00:20:41 +0300

    sm6150-common: Add QTI health implementation
    Add QTI health implementation so that it waits for battery power
    supply for a max timeout of 5 seconds. This is to ensure by the
    time health HAL service starts, battery power supply device is
    available. Without this, health HAL couldn't find battery power
    supply sometimes because of the time taken for battery charger
    driver to probe on platforms where it can happen only after the
    remote subsytem is bought out of reset.

--- sm6150.mk
+++ sm6150.mk
@@ -230,9 +230,7 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1.vendor
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
