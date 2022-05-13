6887d4e54786cea1c0165ef9ec73b585e70525c9
AndroidHQ254 <d.murimi14@gmail.com>
2022-05-14 00:16:55 +0300

    sm6150-common: Add missing seccomp policy
    * W mediaextractor: Could not read additional policy file '/vendor/etc/seccomp_policy/mediaextractor.policy'

--- sm6150.mk
+++ sm6150.mk
@@ -394,7 +394,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
+    $(LOCAL_PATH)/configs/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy \
+    $(LOCAL_PATH)/configs/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \
